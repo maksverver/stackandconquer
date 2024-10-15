6d54a4e6e61830eda369de9619c9193d3cb2ae71
Maks Verver <maks@verver.ch>
2024-10-15 14:42:57 +0200

    Support arbitrary board configurations.

--- data/cpu/MinimaxCPU.js
+++ data/cpu/MinimaxCPU.js
@@ -39,73 +39,119 @@
 
 'use strict';
 
-var log = (typeof game === 'object' && game.log) || console.log;
-
-// These variables define the configuration of the board. The current version of
-// the AI only supports 2 players and the standard 5x5 board configuration.
-//
-// (I currently use a bitmask to represent occupied fields, which limits the
-// maximum number of fields to 31!)
-var ROWS = 5;
-var COLS = 5;
-var FIELD_COUNT = ROWS * COLS;
-var WINNING_HEIGHT = 5;
-var PIECES_PER_PLAYER = 20;
+// Note: unlike console.log(), game.log() supports only a single string argument!
+var log = typeof game === 'object' ? game.log : console.log;
 
 // Minimax search depth.
 //
 // Determines the strength of the AI: higher is better, but slower.
 var SEARCH_DEPTH = 4;
 
-// Data used to generate possible moves quickly.
-//
-// MOVE_TEMPLATES[dst][height] is an array of [src, mask] fields, where
-// mask is a bitmask of fields between src and dst.
-var MOVE_TEMPLATES = generateMoveTemplates();
+// Number of pieces per player. This should be configurable, but currently the
+// game does not pass this information to the CPU players (issue #11).
+var PIECES_PER_PLAYER = 20;
 
 // Move that represents passing (an empty array).
 var PASS = Object.freeze([]);
 
-function generateMoveTemplates() {
+function createConfig(
+    rows, cols, inputFields, outside, padding,
+    winningHeight, piecesPerPlayer, playerCount) {
+  if (inputFields.length !== rows * cols) {
+    throw new Error('Invalid length of input fields');
+  }
+  var apiToFieldIndex = [];
+  var fieldIndexToApi = [];
+  var fieldCount = 0;
+  for (var r1 = 0; r1 < rows; ++r1) {
+    for (var c1 = 0; c1 < cols; ++c1) {
+      var i = cols * r1 + c1;
+      if (inputFields[i] === outside || inputFields[i] === padding) {
+        apiToFieldIndex.push(-1);
+      } else {
+        apiToFieldIndex.push(fieldCount++);
+        fieldIndexToApi.push(i);
+      }
+    }
+  }
+  if (fieldCount > 30) {
+    throw new Error('Too many fields (maximum supported: 30)');
+  }
+
+  // moves[dst][height] is an array of [src, mask] fields, where
+  // mask is a bitmask of fields between src and dst.
   var moves = [];
   var DR = [-1, -1, -1,  0,  0, +1, +1, +1];
   var DC = [-1,  0, +1, -1, +1, -1,  0, +1];
-  for (var r2 = 0; r2 < ROWS; ++r2) {
-    for (var c2 = 0; c2 < COLS; ++c2) {
-      const dst = r2*COLS + c2;
+  for (var r2 = 0; r2 < rows; ++r2) {
+    for (var c2 = 0; c2 < cols; ++c2) {
+      var j = cols * r2 + c2;
+      if (inputFields[j] === outside || inputFields[j] === padding) continue;
+      var dst = moves.length;
       moves.push([]);
-      for (var height = 0; height < WINNING_HEIGHT; ++height) {
+      for (var height = 0; height < winningHeight; ++height) {
         moves[dst].push([]);
       }
       for (var dir = 0; dir < 8; ++dir) {
         var dr = DR[dir], dc = DC[dir];
         var mask = 0;
-        for (var height = 1; height < WINNING_HEIGHT; ++height) {
+        for (var height = 1; height < winningHeight; ++height) {
           var r1 = r2 - dr*height;
           var c1 = c2 - dc*height;
-          if (r1 < 0 || r1 >= ROWS || c1 < 0 || c1 >= COLS) break;
-          var src = r1*COLS + c1;
+          if (r1 < 0 || r1 >= rows || c1 < 0 || c1 >= cols) break;
+          var i = cols * r1 + c1;
+          if (inputFields[i] === outside || inputFields[i] === padding) break;
+          var src = apiToFieldIndex[r1*cols + c1];
           moves[dst][height].push([src, mask]);
           mask |= 1 << src;
         }
       }
     }
   }
-  return moves;
+
+  return Object.freeze({
+    apiToFieldIndex: Object.freeze(apiToFieldIndex),
+    fieldIndexToApi: Object.freeze(fieldIndexToApi),
+    fieldCount: fieldCount,
+    moves: Object.freeze(moves),
+    winningHeight: winningHeight,
+    piecesPerPlayer: piecesPerPlayer,
+    playerCount: playerCount,
+    // These are only used for parseMove() and debug-printing:
+    rows: rows,
+    cols: cols,
+    // Maybe add this to debug-print non-rectangular boards:
+    // inputFields: inputFields,
+  });
+}
+
+function arrayOfValues(len, value) {
+  var res = [];
+  for (var i = 0; i < len; ++i) res.push(value);
+  return res;
+}
+
+function arrayOfObjects(len, constructor) {
+  var fields = [];
+  for (var i = 0; i < len; ++i) fields.push(constructor());
+  return fields;
 }
 
 // Constructs a new state, possibly from an old state returned by toJson().
 // Note that inputJson, if given, MUST be valid, or things will get weird!
-function State(inputJson) {
+function State(cfg, inputJson) {
 
   // Pieces on the board. For each field, we store an array of colors 0 and 1.
-  var fields = inputJson ? inputJson.fields : [];
+  var fields = inputJson ? inputJson.fields : arrayOfObjects(cfg.fieldCount, Array);
 
   // Next player to move. Either 0 or 1.
   var nextPlayer = inputJson ? inputJson.nextPlayer : 0;
 
-  // Scores for each player. TODO later: support third player.
-  var scores = inputJson ? inputJson.scores : [0, 0];
+  // Scores for each player (i.e., number of towers won).
+  //
+  // Ideally I would represent this as an array of number of towers left to win,
+  // but the game does not pass this information to the CPU players (issue #10).
+  var scores = inputJson ? inputJson.scores : arrayOfValues(cfg.playerCount, 0);
 
   // Last move played (to prevent reverting)
   var lastMove = inputJson ? inputJson.lastMove : null;
@@ -114,12 +160,9 @@ function State(inputJson) {
   var occupied = 0;
 
   // Number of pieces per player.
-  var piecesLeft = [PIECES_PER_PLAYER, PIECES_PER_PLAYER];
+  var piecesLeft = arrayOfValues(cfg.playerCount, cfg.piecesPerPlayer);
 
-  if (!inputJson) {
-    // Initialize empty fields.
-    for (var i = 0; i < FIELD_COUNT; ++i) fields.push([]);
-  } else {
+  if (inputJson) {
     // Recompute `occupied` and `piecesLeft` from given fields.
     for (var i = 0; i < fields.length; ++i) {
       var height = fields[i].length;
@@ -148,7 +191,7 @@ function State(inputJson) {
         if (srcField.length === 0) {
           occupied ^= 1 << src;
         }
-        if (dstField.length >= WINNING_HEIGHT) {
+        if (dstField.length >= cfg.winningHeight) {
           removed = dstField.splice(0);
           var winner = removed[removed.length - 1];
           scores[winner] += 1;
@@ -204,12 +247,12 @@ function State(inputJson) {
       var dstField = fields[dst];
       var dstHeight = dstField.length;
       if (dstHeight > 0) {
-        var options = MOVE_TEMPLATES[dst][dstHeight];
+        var options = cfg.moves[dst][dstHeight];
         for (var i = 0; i < options.length; ++i) {
           var src = options[i][0];
           var srcField = fields[src]
           var srcHeight = srcField.length;
-          if (srcHeight + dstHeight >= WINNING_HEIGHT && (occupied & options[i][1]) === 0) {
+          if (srcHeight + dstHeight >= cfg.winningHeight && (occupied & options[i][1]) === 0) {
             if (srcField[srcHeight - 1] === nextPlayer) {
               // Winning move found!
               score += 1000;
@@ -244,7 +287,9 @@ function State(inputJson) {
   // A move is a triple [cnt, src, dst], or an empty array [] to pass.
   // If cnt == 1 and src == dst, a new piece is placed.
   //
-  // Rules of the game: https://spielstein.com/games/mixtour/rules
+  // Rules of the game:
+  //  - https://spielstein.com/games/mixtour/rules (2 players)
+  //  - https://spielstein.com/games/mixtour/rules/a-trois (3 players)
   function generateMoves() {
     var moves = [];
     var lastCnt = 0, lastSrc = -1, lastDst = -1;
@@ -260,7 +305,7 @@ function State(inputJson) {
           moves.push([1, dst, dst]);  // place new piece
         }
       } else {
-        var options = MOVE_TEMPLATES[dst][dstHeight];
+        var options = cfg.moves[dst][dstHeight];
         for (var i = 0; i < options.length; ++i) {
           var src = options[i][0];
           var srcHeight = fields[src].length;
@@ -279,14 +324,17 @@ function State(inputJson) {
   }
 
   // Logs the current state to standard output in a human-readable format.
+  //
+  // This currently only works for rectangular boards without holes, like the
+  // default 5x5 board.
   function debugPrint() {
     log('Scores: ' + scores);
     log('Pieces left: ' + piecesLeft);
     log('Player ' + (nextPlayer + 1) + ' to move.');
-    for (var r = 0; r < ROWS; ++r) {
+    for (var r = 0; r < cfg.rows; ++r) {
       var line = formatRow(r) + '  ';
-      for (var c = 0; c < COLS; ++c) {
-        var src = r*COLS + c;
+      for (var c = 0; c < cfg.cols; ++c) {
+        var src = r*cfg.cols + c;
         var part = '';
         if (fields[src].length === 0) {
           part = '.';
@@ -295,7 +343,7 @@ function State(inputJson) {
             part += String(fields[src][i] + 1);
           }
         }
-        while (part.length < WINNING_HEIGHT) part += ' ';
+        while (part.length < cfg.winningHeight) part += ' ';
         line += ' ' + part;
         if (((occupied & (1 << src)) !== 0) != (fields[src].length !== 0)) {
           log('INTERNAL ERROR: occupied does not match fields at ' + src);
@@ -304,15 +352,15 @@ function State(inputJson) {
       log(line);
     }
     var line = '   ';
-    for (var c = 0; c < COLS; ++c) {
+    for (var c = 0; c < cfg.cols; ++c) {
       var part = formatCol(c);
-      while (part.length < WINNING_HEIGHT) part += ' ';
+      while (part.length < cfg.winningHeight) part += ' ';
       line += ' ' + part;
     }
     log(line);
-    log('last move: ' + (lastMove ? formatMove(lastMove) : 'none'));
-    var moves = generateMoves();
-    log(moves.length + ' possible moves: ' + formatMoves(moves));
+    log('last move: ' + (lastMove ? formatMove(cfg, lastMove) : 'none'));
+    var moves = generateMoves(cfg.moves);
+    log(moves.length + ' possible moves: ' + formatMoves(cfg, moves));
   }
 
   // Returns the state as a JSON-serializable object. This does not do a deep
@@ -401,67 +449,72 @@ function indexOfMove(moves, move) {
 /////////////////////////////////////////
 
 function initCPU() {
-  // Currently we only support one game configuration.
-  if (game.getBoardDimensionX() !== COLS ||
-      game.getBoardDimensionY() !== ROWS ||
-      game.getHeightToWin() !== WINNING_HEIGHT ||
-      game.getNumOfPlayers() !== 2) {
-    throw new Error('Unsupported game configuration!');
+  if (game.getNumOfPlayers() !== 2) {
+    throw new Error('Unsupported number of players!');
   }
 }
 
 function callCPU(jsonBoard, jsonMoves, nDirection) {
-  var myId = game.getID();
-  var outside = game.getOutside();
-  var padding = game.getPadding();
-
-  if (myId < 1 || myId > 2) throw new Error('Invalid player id');
-
+  var board = JSON.parse(jsonBoard);
   var legalMoves = JSON.parse(jsonMoves);
+
+  // Possible optimization: cache this between calls to callCPU(), assuming that
+  // the game configuration including board layout does not change in between.
+  var cfg = createConfig(
+    game.getBoardDimensionY() + 2 * game.getHeightToWin(),
+    game.getBoardDimensionX() + 2 * game.getHeightToWin(),
+    board,
+    game.getOutside(),
+    game.getPadding(),
+    game.getHeightToWin(),
+    PIECES_PER_PLAYER,
+    game.getNumOfPlayers(),
+  );
+
+  var myId = game.getID();
+  if (myId < 1 || myId > cfg.playerCount) throw new Error('Invalid player id');
+
   if (legalMoves.length === 0) throw new Error('No moves available');
   if (legalMoves.length === 1) return legalMoves[0];
 
-  var fieldIndices = [];
-  var fields = [];
-  var board = JSON.parse(jsonBoard);
-  for (var i = 0; i < board.length; ++i) {
-    if (board[i] === outside || board[i] === padding) {
-      fieldIndices.push(-1);
-    } else {
-      fieldIndices.push(fields.length);
-      var field = [];
-      for (var j = 0; j < board[i].length; ++j) {
-        var piece = board[i].charCodeAt(j) - '1'.charCodeAt(0);
-        if (piece < 0 || piece > 1) throw new Error('Invalid piece');
-        field.push(piece);
-      }
-      fields.push(field);
-    }
-  }
-  if (fields.length !== FIELD_COUNT) throw new Error('Invalid number of fields');
-
-  // Convert legal moves from API format. (We need to do this because the game
-  // won't tell us what the last move was, but it is omitted from the legal
-  // moves. Also, my AI allows building a tower that gives a point to the
-  // opponent while the game does not seem to allow this.)
+  // Convert legal moves from API format. We need to do this because the game
+  // won't tell us what the last move was (issue #9), but it is omitted from the
+  // legal moves. Also, my AI allows building a tower that gives a point to the
+  // opponent while the game does not seem to allow this, so using legalMoves as
+  // the source of truth prevents making moves the game considers illegal.
   var moves = [];
   for (var i = 0; i < legalMoves.length; ++i) {
     const apiMove = legalMoves[i];
     if (apiMove[0] === -1) {
-      // Place a new piece: [-1, 1, dst] => [1, dst, st]
-      moves.push([apiMove[1], fieldIndices[apiMove[2]], fieldIndices[apiMove[2]]]);
+      // Place a new piece: [-1, 1, dst] => [1, dst, dst]
+      moves.push([apiMove[1], cfg.apiToFieldIndex[apiMove[2]], cfg.apiToFieldIndex[apiMove[2]]]);
     } else {
       // Move a stack: [src, cnt, dst] => [cnt, src, dst]
-      moves.push([apiMove[1], fieldIndices[apiMove[0]], fieldIndices[apiMove[2]]]);
+      moves.push([apiMove[1], cfg.apiToFieldIndex[apiMove[0]], cfg.apiToFieldIndex[apiMove[2]]]);
     }
   }
 
-  var state = new State({fields: fields, nextPlayer: myId - 1, scores: [0, 0], lastMove: null});
+  var fields = [];
+  for (var i = 0; i < cfg.fieldCount; ++i) {
+    var field = [];
+    var string = board[cfg.fieldIndexToApi[i]];
+    for (var j = 0; j < string.length; ++j) {
+      var piece = string.charCodeAt(j) - '1'.charCodeAt(0);
+      if (piece < 0 || piece >= cfg.playerCount) throw new Error('Invalid piece');
+      field.push(piece);
+    }
+    fields.push(field);
+  }
+  if (fields.length !== cfg.fieldCount) throw new Error('Invalid number of fields');
+
+  var scores = arrayOfValues(cfg.playerCount, 0);  // issue #10
+  var lastMove = null;  // issue #9
+  var state = State(cfg, {fields: fields, nextPlayer: myId - 1, scores: scores, lastMove: lastMove});
   var result = findBestMoves(state, moves);
   var bestMoves = result[0];
   var bestMove = bestMoves[Math.floor(Math.random() * bestMoves.length)];
-  log(bestMoves.length + ' best move(s) with value ' + result[1] + ': ' +
-        formatMoves(bestMoves) + '; choosing ' + formatMove(bestMove) + ' at random');
+  log(moves.length + ' moves, ' + bestMoves.length + ' best with value ' + result[1] + ': ' +
+        formatMoves(cfg, bestMoves) + '; selected ' + formatMove(cfg, bestMove) + ' at random');
   var moveIndex = indexOfMove(moves, bestMove);
   if (moveIndex < 0) throw new Error('Best move not found somehow?!');
   return legalMoves[moveIndex];
@@ -486,7 +539,7 @@ function callCPU(jsonBoard, jsonMoves, nDirection) {
 //
 // If the move cannot be parsed, undefined is returned. This function DOES NOT
 // validate the move beyond checking the coordinates are valid.
-function parseMove(move) {
+function parseMove(cfg, move) {
   if (move === 'pass') return [];
   if (move.length < 2 || move.length > 5) return undefined;
   var i = 0;
@@ -495,8 +548,8 @@ function parseMove(move) {
   var r1 = move.charCodeAt(i++) - '1'.charCodeAt(0);
   var c2 = i < move.length ? move.charCodeAt(i++) - 'a'.charCodeAt(0) : c1;
   var r2 = i < move.length ? move.charCodeAt(i++) - '1'.charCodeAt(0) : r1;
-  if (cnt > 0 && cnt < WINNING_HEIGHT && r1 >= 0 && r1 < ROWS && c1 >= 0 && c1 < COLS) {
-    return [cnt, r1*ROWS + c1, r2*ROWS + c2];
+  if (cnt > 0 && cnt < cfg.winningHeight && r1 >= 0 && r1 < cfg.rows && c1 >= 0 && c1 < cfg.cols) {
+    return [cnt, r1*cfg.rows + c1, r2*cfg.rows + c2];
   }
 }
 
@@ -508,12 +561,13 @@ function formatCol(col) {
   return String.fromCharCode('a'.charCodeAt(0) + col);
 }
 
-function formatMove(move) {
+function formatMove(cfg, move) {
   if (move.length === 0) return 'pass';
-  var c1 = move[1] % COLS;
-  var r1 = (move[1] - c1) / COLS;
-  var c2 = move[2] % COLS;
-  var r2 = (move[2] - c2) / COLS;
+  var cols = cfg.cols;
+  var c1 = move[1] % cols;
+  var r1 = (move[1] - c1) / cols;
+  var c2 = move[2] % cols;
+  var r2 = (move[2] - c2) / cols;
   var res = '';
   if (move[0] != 1) res += String(move[0]);
   res += formatCol(c1) + formatRow(r1);
@@ -521,11 +575,11 @@ function formatMove(move) {
   return res;
 }
 
-function formatMoves(moves) {
+function formatMoves(cfg, moves) {
   var res = '';
   for (var i = 0; i < moves.length; ++i) {
     if (i > 0) res += ' ';
-    res += formatMove(moves[i]);
+    res += formatMove(cfg, moves[i]);
   }
   return res;
 }
@@ -546,10 +600,14 @@ function formatMoves(moves) {
 //          3a1b2 (move 3 pieces from a1 to b2)
 //
 function runStandalone() {
+  var cfg = createConfig(5, 5, ".........................", '#', '-', 5, 20, 2);
+
   var undoStack = [];
   var redoStack = [];
-  var state = State();
+  var state = State(cfg);
   state.debugPrint();
+
+  var currentLineHandler = defaultLineHandler;
 
   function defaultLineHandler(line) {
     if (line === 'save') {
@@ -579,9 +637,9 @@ function runStandalone() {
       log(state.evaluate());
     } else if (line === 'best') {
       var result = findBestMoves(state, state.generateMoves());
-      log(result[0].length + ' best move(s) with value ' + result[1] + ': ' + formatMoves(result[0]));
+      log(result[0].length + ' best move(s) with value ' + result[1] + ': ' + formatMoves(cfg, result[0]));
     } else {
-      var move = parseMove(line);
+      var move = parseMove(cfg, line);
       if (move == null) {
         log('Could not parse move!');
       } else if (indexOfMove(state.generateMoves(), move) < 0) {
@@ -598,7 +656,7 @@ function runStandalone() {
   function loadLineHandler(line) {
     currentLineHandler = defaultLineHandler;
     try {
-      var newState = State(JSON.parse(line));
+      var newState = State(cfg, JSON.parse(line));
       newState.debugPrint();
       // Order matters here. Only overwrite state if debugPrint() succeeds,
       // so that if the state is invalid, we keep the old state.
@@ -616,7 +674,6 @@ function runStandalone() {
   var rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout});
-  var currentLineHandler = defaultLineHandler;
   rl.on('line', (line) => currentLineHandler(line));
   rl.once('close', () => log('EOF'));
 }
@@ -631,6 +688,5 @@ if (typeof game === 'undefined') {
 //  - add tests
 //  - add benchmarks (before attempting further optimizations)
 //  - optimize search and evaluation functions
-//  - support arbitrary board configurations
 //  - support more than 2 players
 //
